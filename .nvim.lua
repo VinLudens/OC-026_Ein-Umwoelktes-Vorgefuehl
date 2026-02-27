3b598ca0b102c7516f5a4bcbc8cc5ac34140f60d
niveK77pur <10981161+niveK77pur@users.noreply.github.com>
2026-02-27 20:26:05 +0000

    Apply automatic changes

--- .nvim.lua
+++
@@ -1,8 +0,0 @@
-require('nvls').setup({
-    lilypond = {
-        options = {
-            main_file = 'Ein-Umwoelktes-Vorgefuehl.ly',
-            include_dir = './openlilylib',
-        },
-    },
-})
